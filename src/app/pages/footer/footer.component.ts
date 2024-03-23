f9d24acceb3672fdb590a1470e4f81b6b0b21772
tuanhv4287/hoc-tieng-han <tuandesigner4287@gmail.com>
2024-03-23 17:43:57 +0700

    edit function next and prevert

--- src/app/pages/footer/footer.component.ts
+++ src/app/pages/footer/footer.component.ts
@@ -3,18 +3,18 @@ import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-  @Output() event: EventEmitter<any> = new EventEmitter;
-  @Output() eventShowMenu: EventEmitter<any> = new EventEmitter;
+  @Output() event: EventEmitter<any> = new EventEmitter();
+  @Output() eventShowMenu: EventEmitter<any> = new EventEmitter();
   previous() {
-    this.event.emit('previous')
+    this.event.emit('previous');
   }
   next() {
-    this.event.emit('next')
+    this.event.emit('next');
   }
   showFooterMenu() {
-    this.eventShowMenu.emit(true)
+    this.eventShowMenu.emit(true);
   }
 }
